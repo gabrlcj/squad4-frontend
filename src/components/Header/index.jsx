9f468fabb1e0bfb547d04a04b60cff3e2a08a5f9
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-03 02:07:06 -0300

    :construction: Componente em construção

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom'
 import { Container } from './styles'
 
 export function Header() {
   return (
     <Container>
       <h1>Header aqui</h1>
+      <nav>
+        <Link to='/'>Página de Login</Link>
+        <Link to='/register'>Página de Cadastro</Link>
+      </nav>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
   background: var(--alternative);
   height: 6rem;
 
@@ -8,5 +11,14 @@ export const Container = styled.header`
     display: flex;
     align-items: center;
     padding: 1.5rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--background);
+  }
+
+  a:first-child {
+    margin-right: 1rem;
   }
 `
