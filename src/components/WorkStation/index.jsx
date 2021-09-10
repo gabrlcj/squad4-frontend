98250a1daab0a0cd6f0a410d225f6f7db8499a9d
Felipe Lobo <felipeblobo@gmail.com>
2021-09-10 09:12:12 -0300

    refatorando estacoes de trabalho

--- src/components/WorkStation/index.jsx
+++ src/components/WorkStation/index.jsx
@@ -1,73 +1,47 @@
-import Table from '../../assets/Table.svg'
-import { Container, Station } from './styles'
+import Table from "../../assets/Table.svg";
+import { Container, Station } from "./styles";
+
+let chairs = ["A", "B", "C", "D", "E", "F", "G", "H"];
+let stationChairs = [];
+let stationChairs2 = [];
+
+chairs.forEach((chair, index) => {
+  if (index < 4) {
+    stationChairs.push(
+      <div key={index + chair} className="circle up">
+        {chair + index}
+      </div>
+    );
+  } else {
+    stationChairs2.push(
+      <div key={index + chair} className="circle up">
+        {chair + index}
+      </div>
+    );
+  }
+});
+
+function tableStation(id) {
+  return (
+    <Station id={id}>
+      <div className="chairs">{stationChairs}</div>
+      <img src={Table} alt="Mesa de trabalho" />
+      <div className="chairs">{stationChairs2}</div>
+    </Station>
+  );
+}
 
 export function WorkStation() {
   return (
     <Container>
-      <div className='content'>
-        <Station>
-          <div className='chairs'>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-          </div>
-          <img src={Table} alt='Mesa de trabalho' />
-          <div className='chairs'>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-          </div>
-        </Station>
-        <Station>
-          <div className='chairs'>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-          </div>
-          <img src={Table} alt='Mesa de trabalho' />
-          <div className='chairs'>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-          </div>
-        </Station>
+      <div className="content">
+        {tableStation(1)}
+        {tableStation(2)}
       </div>
-      <div className='content'>
-        <Station>
-          <div className='chairs'>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-          </div>
-          <img src={Table} alt='Mesa de trabalho' />
-          <div className='chairs'>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-          </div>
-        </Station>
-        <Station>
-          <div className='chairs'>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-            <div className='circle up'></div>
-          </div>
-          <img src={Table} alt='Mesa de trabalho' />
-          <div className='chairs'>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-            <div className='circle down'></div>
-          </div>
-        </Station>
+      <div className="content">
+        {tableStation(3)}
+        {tableStation(4)}
       </div>
     </Container>
-  )
+  );
 }
