d364b35670c1c8b4bd026539798d14df687bfeaa
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-10 17:24:00 -0300

    fix: passando valor dos days para o context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
-const AuthContext = createContext({});
+const AuthContext = createContext({})
 
 const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
-  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null)
+  const [user, setUser] = useState(null)
+  const [day, setDay] = useState(new Date())
 
-  return (
-    <AuthContext.Provider value={{ token, setToken, user, setUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+  return <AuthContext.Provider value={{ token, setToken, user, setUser, day, setDay }}>{children}</AuthContext.Provider>
+}
 
-export { AuthContext, AuthProvider };
+export { AuthContext, AuthProvider }
