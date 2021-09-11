ad0831df58f270717d5dc11daae8c2465cf7f182
dansenpir <senegalha@protonmail.com>
2021-09-11 14:37:53 -0300

    Concluindo posicionamento dos elementos.

--- src/components/Questionnaire/index.jsx
+++ src/components/Questionnaire/index.jsx
@@ -9,95 +9,96 @@ export function QuestionnairePage() {
   return (
     <>
       <Container>
-        <h1>Olá, é sua primeira viagem, né?!</h1>
-        <p>Antes de começar, responda essas perguntas para a gente te conhecer melhor!</p>
+        <div className="window">
+          <h1 className="title">Olá!<br />Sua primeira vez por aqui, né?</h1>
+          <p className="title">Antes de começar, responda essas perguntas para a gente te conhecer melhor!</p>
 
-        <form className="box">
-          <div className="left">
+          <form className="box">
+            <div className="left">
 
-            <h3 className="question">Você está vacinado contra a COVID-19?</h3>
-            <div className="box-question">
-              <div>
-                <input id="vaccine_true" name='vaccine' type='radio' value={vaccine_status} onClick={() => setVaccine(true)} />
-                <label htmlFor="vaccine_true">Sim</label>
+              <h3 className="question">Você está vacinado contra a COVID-19?</h3>
+              <div className="box-question">
+                <div>
+                  <input id="vaccine_true" name='vaccine' type='radio' value={vaccine_status} onClick={() => setVaccine(true)} />
+                  <label htmlFor="vaccine_true">Sim</label>
+                </div>
+
+                <div>
+                  <input id="vaccine_false" name='vaccine' type='radio' value={vaccine_status} onClick={() => setVaccine(false)} />
+                  <label htmlFor="vaccine_false">Não</label>
+                </div>
+
+                <div>
+                  <input id="vaccine_maybe" name='vaccine' type='radio' value={vaccine_status} onClick={() => setVaccine(false)} />
+                  <label htmlFor="vaccine_maybe">Prefiro não responder</label>
+                </div>
               </div>
 
-              <div>
-                <input id="vaccine_false" name='vaccine' type='radio' value={vaccine_status} onClick={() => setVaccine(false)} />
-                <label htmlFor="vaccine_false">Não</label>
+              <h3 className="question">MODELO DE PERGUNTA</h3>
+              <div className="box-question">
+                <div>
+                  <input name='leftExample' type='radio' value="leftExample1" id="leftExample1" />
+                  <label for="leftExample1">Sim</label>
+                </div>
+
+                <div>
+                  <input name='leftExample' type='radio' value="leftExample2" id="leftExample2" />
+                  <label htmlFor="leftExample2">Não</label>
+                </div>
+
+                <div>
+                  <input name='leftExample' type='radio' value="leftExample3" id="leftExample3" />
+                  <label htmlFor="leftExample3">Prefiro não responder</label>
+                </div>
               </div>
 
-              <div>
-                <input id="vaccine_maybe" name='vaccine' type='radio' value={vaccine_status} onClick={() => setVaccine(false)} />
-                <label htmlFor="vaccine_maybe">Prefiro não responder</label>
-              </div>
             </div>
 
-            <h3 className="question">MODELO DE PERGUNTA</h3>
-            <div className="box-question">
-              <div>
-                <input name='leftExample' type='radio' value="leftExample1" id="leftExample1" />
-                <label for="leftExample1">Sim</label>
+            <div className="right">
+
+              <h3 className="question">Para que a gente possa te receber melhor, você faz uso de cadeira de rodas?</h3>
+
+              <div className="box-question">
+                <div>
+                  <input id="wheelchair_true" name='wheelchair' type='radio' value={pwd} onClick={() => setPWD(true)} />
+                  <label htmlFor="wheelchair_true">Sim</label>
+                </div>
+
+                <div>
+                  <input id="wheelchair_false" name='wheelchair' type='radio' value={pwd} onClick={() => setPWD(false)} />
+                  <label htmlFor="wheelchair_false">Não</label>
+                </div>
+
+                <div>
+                  <input id="wheelchair_maybe" name='wheelchair' type='radio' value={pwd} onClick={() => setPWD(false)} />
+                  <label htmlFor="wheelchair_maybe">Prefiro não responder</label>
+                </div>
               </div>
 
-              <div>
-                <input name='leftExample' type='radio' value="leftExample2" id="leftExample2" />
-                <label htmlFor="leftExample2">Não</label>
-              </div>
+              <h3 className="question">MODELO DE PERGUNTA</h3>
+              <div className="box-question">
+                <div>
+                  <input name='rightExample' type='radio' value="rightExample1" id="rightExample1" />
+                  <label for="rightExample1">Sim</label>
+                </div>
 
-              <div>
-                <input name='leftExample' type='radio' value="leftExample3" id="leftExample3" />
-                <label htmlFor="leftExample3">Prefiro não responder</label>
-              </div>
-            </div>
+                <div>
+                  <input name='rightExample' type='radio' value="rightExample2" id="rightExample2" />
+                  <label htmlFor="rightExample2">Não</label>
+                </div>
 
-          </div>
-
-          <div className="right">
-
-            <h3 className="question">Para que a gente possa te receber melhor, você faz uso de cadeira de rodas?</h3>
-
-            <div className="box-question">
-              <div>
-                <input id="wheelchair_true" name='wheelchair' type='radio' value={pwd} onClick={() => setPWD(true)} />
-                <label htmlFor="wheelchair_true">Sim</label>
-              </div>
-
-              <div>
-                <input id="wheelchair_false" name='wheelchair' type='radio' value={pwd} onClick={() => setPWD(false)} />
-                <label htmlFor="wheelchair_false">Não</label>
-              </div>
-
-              <div>
-                <input id="wheelchair_maybe" name='wheelchair' type='radio' value={pwd} onClick={() => setPWD(false)} />
-                <label htmlFor="wheelchair_maybe">Prefiro não responder</label>
-              </div>
-            </div>
-
-            <h3 className="question">MODELO DE PERGUNTA</h3>
-            <div className="box-question">
-              <div>
-                <input name='rightExample' type='radio' value="rightExample1" id="rightExample1" />
-                <label for="rightExample1">Sim</label>
-              </div>
-
-              <div>
-                <input name='rightExample' type='radio' value="rightExample2" id="rightExample2" />
-                <label htmlFor="rightExample2">Não</label>
-              </div>
-
-              <div>
-                <input name='rightExample' type='radio' value="rightExample3" id="rightExample3" />
-                <label htmlFor="rightExample3">Prefiro não responder</label>
+                <div>
+                  <input name='rightExample' type='radio' value="rightExample3" id="rightExample3" />
+                  <label htmlFor="rightExample3">Prefiro não responder</label>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
 
-        {/* <button type='submit'>
-          PROSSEGUIR
-        </button> */}
-
+          <button type='submit'>
+            PROSSEGUIR
+          </button>
+        </div>
       </Container>
     </>
   )
--- src/components/Questionnaire/styles.js
+++ src/components/Questionnaire/styles.js
@@ -1,17 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin: 0 auto;
-  width: 76.5rem;
-  height: auto;
-  background: #F0EDEC;
+  width: min(1024px, 90%);
+  min-height: 100vh;
+  background: #DAD8D7;
 
+  .window {
+    margin: auto;
+    margin-top: 6.875rem;
+    margin-bottom: 6.875rem;
+    background: #F0EDEC;
+    width: 70rem;
+    height: 50rem;
+
+    .title {
+      padding-top: 1.875rem;
+      padding-left: 5.313rem;
+      font-weight: 700;
+      font-style: normal;
+      line-height: 2rem;
+
+      h1 {
+        font-size: 1.5rem;
+      }
+
+      p {
+        font-size: 1.125rem;
+      }
+
+    }
+  }
+  
   .box {
     display: flex;
     flex: 1;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
-    height: 500px;
+    /* padding-top: 2.813rem; */
+
+    .box-question div {
+      display: flex;
+      flex-direction: row-reverse;
+      align-items: center;
+      align-content: center;
+      justify-content: space-between;
+      width: 70%;
+      padding-top: 0.625rem;
+      padding-left: 20%;
+    }
 
     input[type="radio"], label{
       display: inline;
@@ -37,52 +77,40 @@ export const Container = styled.div`
 
   .left {
     width: 40%;
-    height: 21.88rem;
-
-    .box-question div {
-      display: flex;
-      flex-direction: row-reverse;
-      align-items: center;
-      align-content: center;
-      justify-content: space-between;
-      width: 50%;
-    }
-
+    padding-right: 2rem;
+    margin-left: 15.938rem;
   }
 
   .right {
     width: 40%;
-    height: 21.88rem;
-
-    .box-question div {
-      display: flex;
-      flex-direction: row-reverse;
-      align-items: center;
-      align-content: center;
-      justify-content: space-between;
-      width: 50%;
-    }
-
+    padding-left: 2rem;
+    margin-right: 15.938rem;
   }
 
   .question {
     width: 27.19rem;
     height: 5.313rem;
-    /* margin-top: 3.125rem; */
+    margin-top: 2.813rem;
     padding-top: 2rem;
     text-align: center;
+    font-size: 1.125rem;
+    font-weight: 700;
+    font-style: normal;
     background: #C4C4C4;
   }
 
   button {
     border: 0;
-    padding: 0.75rem 2rem;
-    margin: 10px;
     border-radius: 0.625rem;
+    padding: 0.75rem 2rem;
+    /* margin: 10px; */
     font-weight: 700;
     background: #535353;
     color: #ffffff;
-    width: 170px;
+    width: 10.625rem;
+    float: right;
+    margin-top: 4.375rem;
+    margin-right: 6rem;
 
     transition: filter 0.2s;
 
