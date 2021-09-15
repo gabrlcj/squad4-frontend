40c2539c6fbbcf20b675fc0e85cc408d0dc29cda
Felipe Lobo <felipeblobo@gmail.com>
2021-09-15 20:09:48 -0300

    adequando arquivo de rotas

--- src/privateRoutes/Private.jsx
+++ src/privateRoutes/Private.jsx
@@ -2,28 +2,18 @@ import { useContext } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
-function PrivateRoute({ component: Component, ...rest }) {
+function PrivateRoute({ children, ...rest }) {
 
   const { token } = useContext(AuthContext);
 
-  return (
-    <Route
-      {...rest}
-      render={
-        () => (
-          token
-            ? (
-              <Component />
-            ) : (
-              <Redirect
-                to={{
-                  pathname: '/login'
-                }}
-              />
-            ))
-      }
-    />
-  );
-}
+    return (
+      <Route {...rest} render={() => {
+        return token
+          ? children
+          : <Redirect to='/' />
+      }} />
+    )
+ }
+ 
 
 export default PrivateRoute;
