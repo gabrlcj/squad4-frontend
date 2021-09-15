5609d71079e51391e2fbafbf841bbc092b7bce13
Felipe Lobo <felipeblobo@gmail.com>
2021-09-15 14:24:33 -0300

    alterando api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://orange-space.herokuapp.com/api/v1',
 })
 
 api.interceptors.request.use(async (config) => {
